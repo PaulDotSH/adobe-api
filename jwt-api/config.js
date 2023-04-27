268a7f7dc547163580848c6ca8dbf1adbfc4cab8
PaulDotSH <84643540+PaulDotSH@users.noreply.github.com>
2023-04-27 16:22:37 -0700

    quick fix

--- jwt-api/config.js
+++ jwt-api/config.js
@@ -2,38 +2,11 @@
 
 module.exports = {
     credentials: {
-        clientId: "bec443e1f1194ed78eca083310a94f9e", //Application ID (Client ID)
-        technicalAccountId: "5A36772262CEE9130A495FE6@techacct.adobe.com", //Technical account ID
-        orgId: "9DC2338D627015580A495EB9@AdobeOrg", // Org ID
-        clientSecret: "p8e-Z_hbO57ydrhr4ScWT9E36hrhHnalKO2_", //Client Secret
-        metaScopes: "ent_user_sdk", // Metascopes
-        privateKey: "-----BEGIN PRIVATE KEY-----\n" +
-            "MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCNugRNXVQ1+sWk\n" +
-            "hbvdE2gSJ50QK4eRCEJt2wSwv4W86ztJ/oY2xc8RuT7MRJbWYH2oJNYY2XsV1Czy\n" +
-            "yOdkDW91lUd0ZC2DpJuU4YDHceVp7CroXf0iHoNejTISgBAfwO1zhsZiA3X8oxLS\n" +
-            "IKrM9okE/As0JDhfT7krBc2IDv7xi8aiMe3Te5Uu6s5sRf6iBThZxp13aYc1cqEz\n" +
-            "JLVnaRiu5bc1vFNplNLEIl/6q8q2RU6w1JKWPFk3GBx2r4HPzKWc1xREIIDzXjlF\n" +
-            "683WgEfq3s/ZX/v6q1IJo6Dp3O/hBfFnGlldZ1zG0VLSEdcrCMq7yAotrhiwnHEe\n" +
-            "llFNi1hrAgMBAAECggEAA5uui/UjtYAZoGz5C1oBdniX12QlYIj/GHw3J8EO4hr8\n" +
-            "Vm1zBWqfqL1A67H8IZ+XppHAcVkc3yfc9EkdwvYgEdabA5ApEuBNesI+aYio2FUq\n" +
-            "i0hL842NUKQNmBcdFj2ZWsb7rZEeuPyPK4EgM8qzQWI7KkFDrM5P64LPtXZyI4kF\n" +
-            "JMeI8dRRrtHFUxU3/mrqB3UA+iwR+k9fi0M+Grk0cx6Sww4TNey6tcMn1I114N1F\n" +
-            "4pRz4Rh7EfI2OuQ85nLPnNe7fZL9e9+iMNnzGzJfCZaPjGSSLjfXV5sBGe0kw0RN\n" +
-            "27D8ZikoxsNxlQ10gbDfQ22R0+Z9GpK0jpGveCiKoQKBgQDEdJG5VwJjluPfhSFy\n" +
-            "Xf4eJqGpINGt98HxrjbJQo2S4A8ct4ag1STIvszu/K4wzTZTaJIKfppR66rghYBQ\n" +
-            "IgimTsKiY478ZAd/pwFPtnV0y/XWFe1NvKThBBUFbX/hvNACOzyBdnHSV9msGKhI\n" +
-            "kNqshfRAgFF0MuWt1OV22tGx0QKBgQC4rur9hXRzYrx5nAewxGzqBpzXveX27a01\n" +
-            "r9olCukMqvjZB0rhxBqCcUUTgN3LbhfwjeChpvT4WJQrUH9eruJL2amQAW6RGd81\n" +
-            "2T2Jro+FB2d6As7osTMoiD15szGruiLrf/ZWi3exf9zPAFgp8oqF7TzSA2Zzk7Rr\n" +
-            "wez5I3WZewKBgQCPoZZlRksV2tzxr9kbT8MZ8i6hg8ApjAm9FB00Gjd92djSqw+A\n" +
-            "CTWim3UjiZQlU/PTGZIkoBldqYq+Knz8mVVvmgfNSDLjLpMY7lYUHJdVdEVhZ5if\n" +
-            "DDwT07M2y68CsNP3va1oDdv76ITZwRFQNK/cEPfaLLbg2NF5vTyjICrN8QKBgQCr\n" +
-            "mLL03nQl5d7Ta5tlrgy4oW5HssUiWxIuKPeapXmGcLGJDZKRFOHAw7f3+Cucoe2S\n" +
-            "cHEAIh3JRpxdZo0CD6Jp8ZLwH7R5E73mzbO04sGtTATkRAm2ATdUeMoN8ua69dcO\n" +
-            "U7YyEaIkBXbHyz0ftKDq8VQqmkguKVZhLS2fC0hwUwKBgQCDgfp0wNOhtgm2AhsX\n" +
-            "VEVAF3/MnjzJ6Qvo4yoRzjc3/M9ge0NWj3B7pGA9vi6Wkocfv4IioDJ7KQXrSv4J\n" +
-            "9WsHbyKylQ9MPCr436vE+zIwDueOFLO+JP+LjOpkPuK3Azvgi6b4vgboJ7+MXyjm\n" +
-            "MhjjK/XYgWsi7sj2DRIuEofU+g==\n" +
-            "-----END PRIVATE KEY-----\n"
+        clientId: "", //Application ID (Client ID)
+        technicalAccountId: "", //Technical account ID
+        orgId: "", // Org ID
+        clientSecret: "", //Client Secret
+        metaScopes: "", // Metascopes
+        privateKey: ""
     }
 }
